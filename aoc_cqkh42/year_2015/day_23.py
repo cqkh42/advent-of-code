bb898e3b713c74f819c18da24f88b6775aa56b39
cqkh42 <jackcooper93@gmail.com>
2020-12-01 10:01:49 +0000

    solving day 23,24

--- aoc_cqkh42/year_2015/day_23.py
+++ aoc_cqkh42/year_2015/day_23.py
@@ -1,6 +1,58 @@
-def part_a(data):
-    return
+def is_even(num):
+    return not num % 2
 
 
-def part_b(data):
-    return
+def iteration(instr, x, index):
+    if instr[0] == 'jio' and x[instr[1]] == 1:
+        index += instr[2]
+        return x, index
+    elif instr[0] == 'jie' and is_even(x[instr[1]]):
+        index += instr[2]
+        return x, index
+    elif instr[0] == 'jmp':
+        index += int(instr[1])
+        return x, index
+    elif instr[0] == "inc":
+        x[instr[1]] += 1
+    elif instr[0] == 'tpl':
+        x[instr[1]] *= 3
+    elif instr[0] == 'hlf':
+        x[instr[1]] *= 0.5
+
+    index += 1
+    return x, index
+
+def part_a(data, target='b'):
+    instructions = data.split('\n')
+    instructions = [tuple(instr.replace(',', '').replace('+', '').split(' '))
+                    for instr in instructions]
+    instructions = [[i if not i.isnumeric() else int(i) for i in instr] for
+                    instr in instructions]
+
+    registers = {'a': 0, 'b': 0}
+    index = 0
+
+
+
+    while index in range(len(instructions)):
+        instr = instructions[index]
+        registers, index = iteration(instr, registers, index)
+
+    return registers[target]
+
+
+def part_b(data, **_):
+    instructions = data.split('\n')
+    instructions = [tuple(instr.replace(',', '').replace('+', '').split(' '))
+                    for instr in instructions]
+    instructions = [[i if not i.isnumeric() else int(i) for i in instr] for
+                    instr in instructions]
+
+    registers = {'a': 1, 'b': 0}
+    index = 0
+
+    while index in range(len(instructions)):
+        instr = instructions[index]
+        registers, index = iteration(instr, registers, index)
+
+    return registers['b']
--- aoc_cqkh42/year_2015/day_24.py
+++ aoc_cqkh42/year_2015/day_24.py
@@ -1,6 +1,64 @@
+import itertools
+import functools
+import math
+
 def part_a(data):
-    return
+    parcels = data
+    parcels = parcels.split('\n')
+    parcels = [int(parcel) for parcel in parcels]
+
+    def valid_group(group):
+        still_to_sort = [parcel for parcel in parcels if parcel not in group]
+        # can we split this to be two equal sizes
+        group_b = (itertools.combinations(still_to_sort, length) for length in
+                   range(min_length, len(still_to_sort)))
+        group_b = (group for group in itertools.chain.from_iterable(group_b) if
+                   sum(group) == each_weight)
+        return any(group_b)
+
+    each_weight = sum(parcels) / 3
+
+    for fewest in range(1, len(parcels)):
+        if sum(parcels[-fewest:]) >= each_weight:
+            break
+
+    group_a = (itertools.combinations(parcels, length) for length in
+               range(fewest, (len(parcels) // 3) + 1))
+    group_a = [group for group in itertools.chain.from_iterable(group_a) if
+               sum(group) == each_weight]
+
+    min_length = min({len(group) for group in group_a})
+    group_a = (group for group in group_a if
+               len(group) == min_length and valid_group(group))
+    return min(math.prod(group) for group in group_a)
 
 
-def part_b(data):
-    return
+def part_b(data, **_):
+    parcels = data
+    parcels = parcels.split('\n')
+    parcels = [int(parcel) for parcel in parcels]
+
+    def valid_group(group):
+        still_to_sort = [parcel for parcel in parcels if parcel not in group]
+        # can we split this to be two equal sizes
+        group_b = (itertools.combinations(still_to_sort, length) for length in
+                   range(min_length, len(still_to_sort)))
+        group_b = (group for group in itertools.chain.from_iterable(group_b) if
+                   sum(group) == each_weight)
+        return any(group_b)
+
+    each_weight = sum(parcels) / 4
+
+    for fewest in range(1, len(parcels)):
+        if sum(parcels[-fewest:]) >= each_weight:
+            break
+
+    group_a = (itertools.combinations(parcels, length) for length in
+               range(fewest, (len(parcels) // 4) + 1))
+    group_a = [group for group in itertools.chain.from_iterable(group_a) if
+               sum(group) == each_weight]
+
+    min_length = min({len(group) for group in group_a})
+    group_a = (group for group in group_a if
+               len(group) == min_length and valid_group(group))
+    return min(math.prod(group) for group in group_a)
