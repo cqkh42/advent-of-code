56d5182cc6c0ae2a64785dedf6d25a39e9815f36
cqkh42 <jackcooper93@gmail.com>
2020-12-01 09:56:47 +0000

    solving day 17

--- aoc_cqkh42/year_2015/day_17.py
+++ aoc_cqkh42/year_2015/day_17.py
@@ -6,17 +6,17 @@ def _increasing_combinations(item):
     return itertools.chain.from_iterable(itertools.combinations(item, length) for length in range(1, len(item) + 1))
 
 
-def part_a(data):
+def part_a(data, target=150):
     containers = [int(container) for container in data.split('\n')]
     container_combinations = _increasing_combinations(containers)
-    valid_containers = (sum(group) == 150 for group in container_combinations)
+    valid_containers = (sum(group) == target for group in container_combinations)
     return sum(valid_containers)
 
 
-def part_b(data, **_):
+def part_b(data, target=150, **_):
     containers = [int(container) for container in data.split('\n')]
     container_combinations = _increasing_combinations(containers)
-    valid_containers = [len(group) for group in container_combinations if sum(group) == 150]
+    valid_containers = [len(group) for group in container_combinations if sum(group) == target]
     lowest = min(valid_containers)
     return valid_containers.count(lowest)
 
