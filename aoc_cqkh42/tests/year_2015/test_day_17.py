fb7f3349f3550caa1f4ac356e9f45ac77a06bb7c
cqkh42 <jackcooper93@gmail.com>
2020-12-01 10:02:04 +0000

    solving day 23,24

--- aoc_cqkh42/tests/year_2015/test_day_17.py
+++ aoc_cqkh42/tests/year_2015/test_day_17.py
@@ -2,10 +2,10 @@ from aoc_cqkh42.year_2015 import day_17
 
 
 def test_part_a():
-    data = '20,15,10,5,5'
+    data = '20\n15\n10\n5\n5'
     assert day_17.part_a(data, target=25) == 4
 
 
 def test_part_b():
-    data = '20,15,10,5,5'
+    data = '20\n15\n10\n5\n5'
     assert day_17.part_b(data, target=25) == 3
--- aoc_cqkh42/tests/year_2015/test_day_24.py
+++ aoc_cqkh42/tests/year_2015/test_day_24.py
@@ -2,10 +2,10 @@ from aoc_cqkh42.year_2015 import day_24
 
 
 def test_part_a():
-    data = '\n'.join([1,2,3,4,5,7,8,9,10,11])
+    data = '1\n2\n3\n4\n5\n7\n8\n9\n10\n11'
     assert day_24.part_a(data) == 99
 
 
 def test_part_b():
-    data = '\n'.join([1,2,3,4,5,7,8,9,10,11])
+    data = '1\n2\n3\n4\n5\n7\n8\n9\n10\n11'
     assert day_24.part_b(data) == 44
