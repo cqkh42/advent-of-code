7ed809db37f1f6d7f8fc7959db77a4944754d4d2
cqkh42 <jackcooper93@gmail.com>
2020-12-01 10:03:20 +0000

    solving day 25

--- aoc_cqkh42/year_2015/day_25.py
+++ aoc_cqkh42/year_2015/day_25.py
@@ -1,6 +1,22 @@
+import re
+
+
 def part_a(data):
-    return
+    row, col = re.findall(r'\d+', data)
+    row = int(row)
+    col = int(col)
+
+    triangle_number = col + row - 1
+    that_number = triangle_number * (triangle_number + 1) / 2
+    my_number = int(that_number - row)
+
+    start = 20151125
+    for _ in range(my_number):
+        start *= 252533
+        start %= 33554393
+
+    return start
 
 
-def part_b(data):
-    return
+def part_b(**_):
+    return None
