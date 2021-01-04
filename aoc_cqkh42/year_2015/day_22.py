7333185670e158aab9977b28c78ee46500cd3f41
cqkh42 <jackcooper93@gmail.com>
2021-01-04 10:28:53 +0000

    tidying 2015 day 22

--- aoc_cqkh42/year_2015/day_22.py
+++ aoc_cqkh42/year_2015/day_22.py
@@ -1,40 +1,29 @@
 from dataclasses import dataclass
 
-
-class Player:
-    def __init__(self, health, mana, armour):
-        self.health = health
-        self._mana = mana
-        self.armour = armour
-        self.used_mana = 0
-
-    @property
-    def mana(self):
-        return self._mana
-
-    @mana.setter
-    def mana(self, mana):
-        self.used_mana += (self._mana - mana)
-        self.mana = mana
+import parse
 
 
-
+PLAYER_HEALTH = 50
+PLAYER_MANA = 500
+EFFECT_STATS = {
+    # (duration, mana)
+    'shield': (6, 113),
+    'poison': (6, 173),
+    'recharge': (5, 229)
+}
 
 @dataclass
 class State:
     player_health: int
     boss_health: int
     mana: int
-    used_mana: int
-    player_armor: int
-    effects: dict
     boss_damage: int
+    used_mana: int = 0
+    poison: int = 0
+    recharge: int = 0
+    shield: int = 0
 
-    EFFECT_STATS = {
-        'shield': {'duration': 6, 'mana': 113},
-        'poison': {'duration': 6, 'mana': 173},
-        'recharge': {'duration': 5, 'mana': 229}
-    }
+    player_armor = 0
 
     def change(self, **kwargs):
         d = {
@@ -42,40 +31,29 @@ class State:
             "boss_health": self.boss_health,
             "mana": self.mana,
             "used_mana": self.used_mana,
-            "player_armor": self.player_armor,
-            "effects": self.effects,
-            "boss_damage": self.boss_damage
+            "boss_damage": self.boss_damage,
+            'poison': self.poison,
+            'recharge': self.recharge,
+            'shield': self.shield
         }
         d.update(kwargs)
         state = State(**d)
         if state.mana >= 0 and state.player_health > 0:
             return state
 
-    def use_mana(self, mana):
-        state = self.change(
-            mana=self.mana - mana,
-            used_mana=self.used_mana + mana
-        )
-        return state
-
-    def add_effect(self, name):
-        if name not in self.effects:
-            stats = self.EFFECT_STATS[name]
-            new = {name: stats['duration']}
-            new.update(self.effects)
-            return self.change(
-                effects=new,
-                mana=self.mana - stats['mana'],
-                used_mana=self.used_mana + stats['mana']
-            )
+    def add_effect(self, name, duration, mana):
+        to_change = {name: duration, 'mana':self.mana-mana, 'used_mana': self.used_mana + mana}
+        if not getattr(self, name):
+            return self.change(**to_change)
 
     def buff(self):
-        self.boss_health = self.boss_health - 3 * (
-                'poison' in self.effects)
-        self.mana = self.mana + 101 * ('recharge' in self.effects)
-        self.player_armor = 7 * ('shield' in self.effects)
-        self.effects = {effect: turns - 1 for effect, turns in
-                        self.effects.items() if turns > 1}
+        self.boss_health -= 3 * (self.poison > 0)
+        self.mana += 101 * (self.recharge > 0)
+        self.player_armor = 7 * (self.shield > 0)
+
+        self.poison = max(self.poison-1, 0)
+        self.recharge = max(self.recharge-1, 0)
+        self.shield = max(self.shield-1, 0)
 
     def cast(self, mana, boss_change=0, player_change=0):
         state = self.change(
@@ -90,70 +68,56 @@ class State:
         self.player_health -= self.boss_damage
         self.player_health += self.player_armor
 
+
 def part_a(data):
-    player_health = 50
-    player_mana = 500
-    boss_stats = data.split('\n')
-    boss_stats = dict(stat.split(': ') for stat in boss_stats)
-    boss_stats = {k: int(v) for k, v in boss_stats.items()}
-    boss_health = boss_stats['Hit Points']
-    boss_damage = boss_stats['Damage']
+    boss_health, boss_damage = parse.parse('Hit Points: {:d}\nDamage: {:d}', data)
 
     cheapest_victory = float('inf')
-    states = [State(player_health, boss_health, player_mana, 0, 0, {}, boss_damage)]
+    states = [
+        State(PLAYER_HEALTH, boss_health, PLAYER_MANA, boss_damage)
+    ]
 
     for state in states:
-        state.buff()
-        casts = (state.cast(*stats) for stats in [(53, -4, 0), (73, -2, +2)])
-        effects = (state.add_effect(name) for name in
-                   ['shield', 'poison', 'recharge'])
-        new_states = (*casts, *effects)
-        new_states = (state for state in new_states if state and state.used_mana < cheapest_victory)
-
-        for new_state in new_states:
-            new_state.buff()
-            new_state.boss_attack()
-            if new_state.boss_health <= 0:
-                cheapest_victory = min(cheapest_victory, new_state.used_mana)
-                continue
-            if new_state.player_health > 0:
-                states.append(new_state)
+        z, cheapest_victory = mm(state, cheapest_victory)
+        states.extend(z)
 
     return cheapest_victory
 
 
-def part_b(data, **_):
-    player_health = 50
-    player_mana = 500
+def mm(state, cheapest_victory):
+    states = []
+    state.buff()
+    casts = (state.cast(*stats) for stats in [(53, -4, 0), (73, -2, +2)])
+    effects = (state.add_effect(name, duration, mana) for name, (duration, mana) in
+               EFFECT_STATS.items())
+    new_states = (*casts, *effects)
+    new_states = (
+        state for state in new_states
+        if state and state.used_mana < cheapest_victory
+    )
 
-    boss_stats = data.split('\n')
-    boss_stats = dict(stat.split(': ') for stat in boss_stats)
-    boss_stats = {k: int(v) for k, v in boss_stats.items()}
-    boss_health = boss_stats['Hit Points']
-    boss_damage = boss_stats['Damage']
+    for new_state in new_states:
+        new_state.buff()
+        new_state.boss_attack()
+        if new_state.boss_health <= 0:
+            cheapest_victory = min(cheapest_victory, new_state.used_mana)
+            continue
+        if new_state.player_health > 0:
+            states.append(new_state)
+    return states, cheapest_victory
+
+def part_b(data, **_):
+    boss_health, boss_damage = parse.parse('Hit Points: {:d}\nDamage: {:d}', data)
 
     cheapest_victory = float('inf')
-    states = [State(player_health, boss_health, player_mana, 0, 0, {}, boss_damage)]
+    states = [
+        State(PLAYER_HEALTH, boss_health, PLAYER_MANA, boss_damage)
+    ]
 
     for state in states:
         state = state.change(player_health=state.player_health - 1)
         if not state:
             continue
-        state.buff()
-
-        casts = (state.cast(*stats) for stats in [(53, -4, 0), (73, -2, +2)])
-        effects = (state.add_effect(name) for name in
-                   ['shield', 'poison', 'recharge'])
-        new_states = (*casts, *effects)
-        new_states = (state for state in new_states if state and state.used_mana < cheapest_victory)
-
-        for new_state in new_states:
-            new_state.buff()
-            new_state.boss_attack()
-            if new_state.boss_health <= 0:
-                cheapest_victory = min(cheapest_victory, new_state.used_mana)
-                continue
-            if new_state.player_health > 0:
-                states.append(new_state)
-
+        z, cheapest_victory = mm(state, cheapest_victory)
+        states.extend(z)
     return cheapest_victory
