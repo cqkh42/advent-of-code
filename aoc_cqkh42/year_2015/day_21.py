c86052cac321484202ba43ed9ac6d0768052efee
cqkh42 <jackcooper93@gmail.com>
2021-01-11 20:08:57 +0000

    using priority queues

--- aoc_cqkh42/year_2015/day_21.py
+++ aoc_cqkh42/year_2015/day_21.py
@@ -1,10 +1,11 @@
+from dataclasses import dataclass, field
 import itertools
-from dataclasses import dataclass
+import queue
+import math
 
 
 @dataclass
 class Item:
-    name: str
     cost: int
     damage: int
     armor: int
@@ -12,91 +13,98 @@ class Item:
 
 # noinspection SpellCheckingInspection
 weapons = [
-    Item('dagger', 8, 4, 0),
-    Item('shortsword', 10, 5, 0),
-    Item('warhammer', 25, 6, 0),
-    Item('longsword', 40, 7, 0),
-    Item('greataxe', 74, 8, 0)
+    Item(8, 4, 0),
+    Item(10, 5, 0),
+    Item(25, 6, 0),
+    Item(40, 7, 0),
+    Item(74, 8, 0)
 ]
+
 
 # noinspection SpellCheckingInspection
 armors = [
-    Item('leather', 13, 0, 1),
-    Item('chainmail', 31, 0, 2),
-    Item('splintmail', 53, 0, 3),
-    Item('bandedmail', 75, 0, 4),
-    Item('platemail', 102, 0, 5),
-    Item('none', 0, 0, 0)
+    Item(13, 0, 1),
+    Item(31, 0, 2),
+    Item(53, 0, 3),
+    Item(75, 0, 4),
+    Item(102, 0, 5),
+    Item(0, 0, 0)
 ]
 
 rings = [
-    Item('damage_+1', 25, 1, 0),
-    Item('damage_+2', 50, 2, 0),
-    Item('damage_+3', 100, 3, 0),
-    Item('defence_+1', 20, 0, 1),
-    Item('defence_+2', 40, 0, 2),
-    Item('defence_+3', 80, 0, 3),
-    Item('none', 0, 0, 0),
-    Item('none', 0, 0, 0)
+    Item(25, 1, 0),
+    Item(50, 2, 0),
+    Item(100, 3, 0),
+    Item(20, 0, 1),
+    Item(40, 0, 2),
+    Item(80, 0, 3),
+    Item(0, 0, 0),
+    Item(0, 0, 0)
 ]
 
 
-def winner(player_stats, boss_stats):
-    player_health = player_stats['Hit Points']
-    boss_health = boss_stats['Hit Points']
-    damage_to_player = max(boss_stats["Damage"] - player_stats["Armor"], 1)
-    damage_to_boss = max(player_stats["Damage"] - boss_stats["Armor"], 1)
+@dataclass(order=True)
+class Items:
+    player_damage: int = field(compare=False)
+    player_armor: int = field(compare=False)
+    boss_health: int = field(compare=False)
+    boss_damage: int = field(compare=False)
+    boss_armor: int = field(compare=False)
+    cost: int
 
-    while True:
-        boss_health -= damage_to_boss
-        if boss_health <= 0:
-            return 'player'
-        player_health -= damage_to_player
-        if player_health <= 0:
-            return 'boss'
+    def winner(self):
+        player_health = 100
+        damage_to_player = max(self.boss_damage - self.player_armor, 1)
+        damage_to_boss = max(self.player_damage - self.boss_armor, 1)
+
+        player_turns_needed = math.ceil(self.boss_health / damage_to_boss)
+        boss_turns_needed = math.ceil(player_health / damage_to_player)
+        return player_turns_needed <= boss_turns_needed
 
 
 def part_a(data):
     boss_stats = data.split('\n')
     boss_stats = dict(stat.split(': ') for stat in boss_stats)
     boss_stats = {k: int(v) for k, v in boss_stats.items()}
+    boss_damage = boss_stats['Damage']
+    boss_health = boss_stats['Hit Points']
+    boss_armor = boss_stats['Armor']
 
-    cost = float('inf')
-    weapon_armor_combos = itertools.product(weapons, armors)
-    ring_options = itertools.combinations(rings, 2)
+    options = queue.PriorityQueue()
 
-    for (weapon, armor), (ring_a, ring_b) in itertools.product(
-            weapon_armor_combos, ring_options):
-        player_stats = {
-            'Damage': weapon.damage + ring_a.damage + ring_b.damage,
-            'Armor': armor.armor + ring_a.armor + ring_b.armor,
-            'Hit Points': 100
-        }
-        if winner(player_stats, boss_stats) == 'player':
-            total_cost = weapon.cost + armor.cost + ring_a.cost + ring_b.cost
-            cost = min(total_cost, cost)
+    for weapon, armor, ring_a, ring_b in itertools.product(
+            weapons, armors, rings, rings):
+        damage = weapon.damage + ring_a.damage + ring_b.damage
+        a = armor.armor + ring_a.armor + ring_b.armor
+        total_cost = weapon.cost + armor.cost + ring_a.cost + ring_b.cost
+        i = Items(damage, a, boss_health, boss_damage, boss_armor, total_cost)
 
-    return cost
+        options.put(i)
+    while options.not_empty:
+        items = options.get()
+        if items.winner():
+            return items.cost
 
 
 def part_b(data, **_):
     boss_stats = data.split('\n')
     boss_stats = dict(stat.split(': ') for stat in boss_stats)
     boss_stats = {k: int(v) for k, v in boss_stats.items()}
+    boss_damage = boss_stats['Damage']
+    boss_health = boss_stats['Hit Points']
+    boss_armor = boss_stats['Armor']
 
-    cost = 0
-    weapon_armor_combos = itertools.product(weapons, armors)
-    ring_options = itertools.combinations(rings, 2)
+    options = queue.PriorityQueue()
 
-    for (weapon, armor), (ring_a, ring_b) in itertools.product(
-            weapon_armor_combos, ring_options):
-        player_stats = {
-            'Damage': weapon.damage + ring_a.damage + ring_b.damage,
-            'Armor': armor.armor + ring_a.armor + ring_b.armor,
-            'Hit Points': 100
-        }
-        if winner(player_stats, boss_stats) == 'boss':
-            total_cost = weapon.cost + armor.cost + ring_a.cost + ring_b.cost
-            cost = max(total_cost, cost)
+    for weapon, armor, ring_a, ring_b in itertools.product(
+            weapons, armors, rings, rings):
+        damage = weapon.damage + ring_a.damage + ring_b.damage
+        a = armor.armor + ring_a.armor + ring_b.armor
+        total_cost = weapon.cost + armor.cost + ring_a.cost + ring_b.cost
+        i = Items(damage, a, boss_health, boss_damage, boss_armor, -total_cost)
 
-    return cost
+        options.put(i)
+    while options.not_empty:
+        items = options.get()
+        if not items.winner():
+            return -items.cost
