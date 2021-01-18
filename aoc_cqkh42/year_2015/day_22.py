d28c46ca8b7d2c4012f2972fd2fe4791d28b7847
cqkh42 <jackcooper93@gmail.com>
2021-01-18 21:26:54 +0000

    tidying

--- aoc_cqkh42/year_2015/day_22.py
+++ aoc_cqkh42/year_2015/day_22.py
@@ -1,5 +1,6 @@
-from copy import deepcopy, copy
-from dataclasses import dataclass
+from copy import copy
+from dataclasses import dataclass, field
+import queue
 
 import parse
 
@@ -14,16 +15,16 @@ EFFECT_STATS = {
 }
 
 
-@dataclass
+@dataclass(order=True)
 class State:
-    player_health: int
-    boss_health: int
-    mana: int
-    boss_damage: int
+    player_health: int=field(compare=False)
+    boss_health: int=field(compare=False)
+    mana: int=field(compare=False)
+    boss_damage: int=field(compare=False)
     used_mana: int = 0
-    poison: int = 0
-    recharge: int = 0
-    shield: int = 0
+    poison: int = field(default=0, compare=False)
+    recharge: int = field(default=0, compare=False)
+    shield: int=field(default=0, compare=False)
 
     player_armor: int = 0
 
@@ -38,8 +39,11 @@ class State:
             setattr(state, name, duration)
             state.mana -= mana
             state.used_mana += mana
-            if state.mana >= 0 and state.player_health > 0:
-                return state
+            return state
+        else:
+            state = copy(self)
+            state.player_health = -100
+            return state
 
     def buff(self):
         self.boss_health -= 3 * (self.poison > 0)
@@ -56,79 +60,73 @@ class State:
         state.boss_health -= boss_change
         state.mana -= mana
         state.used_mana += mana
-        if state.mana >= 0 and state.player_health > 0:
-            return state
+        return state
 
     def boss_attack(self):
         self.player_health -= self.boss_damage - self.player_armor
 
+    def is_valid(self):
+        return self.mana >= 0 and self.player_health > 0
 
-def mm(state, cheapest_victory):
-    states = []
-    state.buff()
-    casts = (state.cast_attack(*stats) for stats in [(53, 4, 0), (73, 2, 2)])
-    effects = (
-        state.cast_effect(name, duration, mana) for
-        name, (duration, mana) in EFFECT_STATS.items()
-    )
-    new_states = (*casts, *effects)
-    new_states = (
-        state for state in new_states
-        if state and state.used_mana < cheapest_victory
-    )
+    def is_complete(self):
+        return self.boss_health <= 0
 
-    for new_state in new_states:
-        new_state.buff()
-        new_state.boss_attack()
-        if new_state.boss_health <= 0:
-            cheapest_victory = min(cheapest_victory, new_state.used_mana)
+    def _next_moves(self):
+        self.buff()
+        casts = (self.cast_attack(*stats) for stats in [(53, 4, 0), (73, 2, 2)])
+        effects = (
+            self.cast_effect(name, duration, mana) for
+            name, (duration, mana) in EFFECT_STATS.items()
+        )
+        new_states = (*casts, *effects)
+        new_states = (state for state in new_states if state.is_valid())
+
+        for new_state in new_states:
+            new_state.buff()
+            new_state.boss_attack()
+            yield new_state
+
+    def next_moves(self):
+        yield from self._next_moves()
+
+
+class StateB(State):
+    def next_moves(self):
+        self.player_health -= 1
+        yield from self._next_moves()
+
+
+def a_star(state):
+    states = queue.PriorityQueue()
+
+    states.put(state)
+    seen = set()
+
+    while states.not_empty:
+        state = states.get()
+        if state in seen:
             continue
-        if new_state.player_health > 0:
-            states.append(new_state)
-    return states, cheapest_victory
+        else:
+            seen.add(state)
+        z = state.next_moves()
+        for i in z:
+            if i.is_complete():
+                return i.used_mana
+            states.put(i)
 
 
 def part_a(data):
-    boss_health, boss_damage = parse.parse('Hit Points: {:d}\nDamage: {:d}', data)
-
-    cheapest_victory = float('inf')
-    states = {
-        State(PLAYER_HEALTH, boss_health, PLAYER_MANA, boss_damage)
-    }
-    seen = set()
-
-    while states:
-        state = states.pop()
-        if state in seen:
-            continue
-        else:
-            seen.add(state)
-        z, cheapest_victory = mm(state, cheapest_victory)
-        z = set(z)
-        states.update(z)
-
-    return cheapest_victory
+    boss_health, boss_damage = parse.parse(
+        'Hit Points: {:d}\nDamage: {:d}', data
+    )
+    starting_state = State(PLAYER_HEALTH, boss_health, PLAYER_MANA, boss_damage)
+    return a_star(starting_state)
 
 
 def part_b(data, **_):
-    boss_health, boss_damage = parse.parse('Hit Points: {:d}\nDamage: {:d}',
-                                           data)
-
-    cheapest_victory = float('inf')
-    states = {
-        State(PLAYER_HEALTH, boss_health, PLAYER_MANA, boss_damage)
-    }
-    seen = set()
-
-    while states:
-        state = states.pop()
-        state.player_health -= 1
-        if state in seen:
-            continue
-        else:
-            seen.add(state)
-        z, cheapest_victory = mm(state, cheapest_victory)
-        z = set(z)
-        states.update(z)
-
-    return cheapest_victory
+    boss_health, boss_damage = parse.parse(
+        'Hit Points: {:d}\nDamage: {:d}', data
+    )
+    starting_state = StateB(PLAYER_HEALTH, boss_health, PLAYER_MANA, boss_damage)
+    starting_state.player_health -= 1
+    return a_star(starting_state)
