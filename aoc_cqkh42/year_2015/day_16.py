e7fff8ca94174a41d435c4022dd86e1964cf8859
cqkh42 <jackcooper93@gmail.com>
2021-01-29 08:35:39 +0000

    2015 is all shiny

--- aoc_cqkh42/year_2015/day_16.py
+++ aoc_cqkh42/year_2015/day_16.py
@@ -1,6 +1,30 @@
 import re
 
+import parse
+
+from aoc_cqkh42 import BaseSolution
+
+p = parse.compile('{:w}: {:d}, {:w}: {:d}, {:w}: {:d}')
+class Solution(BaseSolution):
+    def parse_data(self):
+        sue_list = REGEX.findall(self.data)
+        sue_list = [
+            dict(zip(bits[::2], [int(num) for num in bits[1::2]]))
+            for bits in sue_list
+        ]
+        return sue_list
+
+    def part_a(self):
+        sue_is_good = [all(sue[key] == AUNTIE[key] for key in sue) for sue in
+                       self.parsed_data]
+        return sue_is_good.index(True) + 1
+
+    def part_b(self):
+        is_good_sue = [_good_sue(sue) for sue in self.parsed_data]
+        return is_good_sue.index(True) + 1
+
 REGEX = re.compile(r'Sue \d+: (.*?): (\d+), (.*?): (\d+), (.*?): (\d+)')
+
 # noinspection SpellCheckingInspection
 AUNTIE = {
     'children': 3,
@@ -16,15 +40,6 @@ AUNTIE = {
 }
 
 
-def _parse_sues(data):
-    sue_list = (REGEX.search(sue).groups() for sue in data.split('\n'))
-    sue_list = [
-        dict(zip(bits[::2], [int(num) for num in bits[1::2]]))
-        for bits in sue_list
-    ]
-    return sue_list
-
-
 # noinspection SpellCheckingInspection
 def _good_sue(sue):
     equals = ['children', 'samoyeds', 'akitas', 'vizslas', 'cars', 'perfumes']
@@ -36,15 +51,3 @@ def _good_sue(sue):
     lt = ['pomeranians', 'goldfish']
     lt = all(sue[attr] < AUNTIE[attr] for attr in lt if attr in sue)
     return equals and gt and lt
-
-
-def part_a(data):
-    sues = _parse_sues(data)
-    sue_is_good = [all(sue[key] == AUNTIE[key] for key in sue) for sue in sues]
-    return sue_is_good.index(True) + 1
-
-
-def part_b(data, **_):
-    sues = _parse_sues(data)
-    is_good_sue = [_good_sue(sue) for sue in sues]
-    return is_good_sue.index(True) + 1
