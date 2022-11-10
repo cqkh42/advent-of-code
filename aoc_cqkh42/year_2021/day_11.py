7dc7d79927101ab37ac92ac88e00556e657e1591
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-11-10 21:33:20 +0000

    2021 day 11

--- aoc_cqkh42/year_2021/day_11.py
+++ aoc_cqkh42/year_2021/day_11.py
@@ -1,41 +1,60 @@
+import itertools
+
 import numpy as np
 from scipy.ndimage import generic_filter
 
 from aoc_cqkh42 import BaseSolution
 
 
-def step(arr):
-    # add 1 to everything
-    # increase all neighbours
-    # burn all 10s we just used (not new 10s)
-    out = arr + 1
-
-    f = [[1, 1,1], [1, 0, 1], [1, 1, 1]]
-    while True:
-        neighbours = generic_filter(arr, lambda x: (x == 10).sum(), footprint=f,
-                                mode='constant', output=int, cval=0)
-        new_out = out + neighbours
-        new_out[(out+neighbours > 10) & (neighbours != 0)] = 10
-        print()
-        print(new_out)
-        if (new_out.clip(max=10) == out.clip(max=10)).all():
-            new_out[new_out >= 10] = 0
-            print()
-            print(new_out)
-            return new_out
-        out = new_out
+def step(consortium):
+    flashed = np.zeros_like(consortium)
+    consortium += 1
+    will_flash = consortium > 9
+    while will_flash.any():
+        f = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+        neighbours = generic_filter(will_flash, sum,
+                                    footprint=f,
+                                    mode='constant', output=int,
+                                    cval=0)
+        consortium += neighbours
+        flashed = np.logical_or(flashed, will_flash)
+        will_flash = np.logical_and(consortium > 9, flashed != 1)
+    consortium[consortium > 9] = 0
+    return consortium
 
 
 class Solution(BaseSolution):
-    def part_a(self):
-        total = 0
-        a = [list(line) for line in self.lines]
-        arr = np.array([list(line) for line in self.lines]).astype(int)
-        for i in range(100):
-            arr = step(arr)
-            total += (arr == 0).sum()
-        return total
+    flashes = 0
 
+    def parse_data(self):
+        return np.array([list(line) for line in self.lines]).astype(int)
+
+    def step(self):
+        flashed = np.zeros_like(self.parsed_data)
+        self.parsed_data += 1
+        will_flash = self.parsed_data > 9
+        while will_flash.any():
+            f = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+            neighbours = generic_filter(will_flash, sum,
+                                        footprint=f,
+                                        mode='constant', output=int,
+                                        cval=0)
+            self.parsed_data += neighbours
+            flashed = np.logical_or(flashed, will_flash)
+            will_flash = np.logical_and(self.parsed_data > 9, flashed != 1)
+        self.parsed_data[self.parsed_data > 9] = 0
+        self.flashes += flashed.sum()
+        return self.parsed_data
+
+    def part_a(self):
+        for _ in range(100):
+            self.step()
+        return self.flashes
 
     def part_b(self):
-        ...
+        self.parsed_data = self.parse_data()
+        for step in itertools.count(1):
+            self.step()
+            if self.parsed_data.sum() == 0:
+                return step
+
