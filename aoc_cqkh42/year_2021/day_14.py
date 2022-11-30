e551f8d205a791e61255f0fdc6c0469940eda2bd
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-11-30 17:20:35 +0000

    2021 day 14 is working

--- aoc_cqkh42/year_2021/day_14.py
+++ aoc_cqkh42/year_2021/day_14.py
@@ -1,67 +1,51 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from functools import cache
-
+# old SKNHVVBSCOPFHKFBKSPNVFFPFVBBKPVBCBVHSHHOHVVFVFFPFHHVNBBCBCBBKSPVKSSBPNVOONHFC
+# old = {'V': 12, 'B': 12, 'F': 10, 'H': 9, 'S': 7, 'P': 7, 'K': 6, 'N': 5, 'C': 5, 'O': 4}
+# new = {'B': 12, 'V': 11.5, 'H': 9, 'F': 7, 'S': 7, 'K': 6, 'P': 6, 'C': 5, 'N': 5, 'O': 4}
 from aoc_cqkh42 import BaseSolution
 
 from frozendict import frozendict
 import parse
-import regex
-
-@cache
-def replace(polymer, mapping):
-    new = ''
-    # if it fails here it because we assume we always have a map
-    for a,b in zip(polymer, polymer[1:]):
-        new += a + mapping[a+b]
-    new += polymer[-1]
-    return new
-
-@cache
-def multiple_replace(polymer, mapping, num):
-    for _ in range(num):
-        polymer = replace(polymer, mapping)
-    return polymer
 
 
 class Solution(BaseSolution):
     parser = parse.compile(r'{:l} -> {:l}')
-    ten_map = {}
 
     def parse_data(self):
-        self.polymer = self.lines[0]
+        self.original_polymer = self.lines[0]
         p = frozendict(self.parser.findall(self.data))
-        self.regex = regex.compile('|'.join(p))
+        self.polymer = Counter((a+b for a, b in zip(self.lines[0], self.lines[0][1:])))
         return p
 
-    def rreplace(self, polymer, old):
-        new = old[0] + self.parsed_data[old] + old[1]
-        li = self.polymer.rsplit(old, 1)
-        self.polymer = new.join(li)
+    def run(self):
+        new = defaultdict(int)
+        for (a, b), v in self.polymer.items():
+            l = a + self.parsed_data[a+b]
+            r = self.parsed_data[a+b] + b
+            new[l] += v
+            new[r] += v
+        self.polymer = new
 
-    def replace(self, polymer):
-        matches = self.regex.findall(polymer, overlapped=True)
-        for old in reversed(matches):
-            self.rreplace(self.polymer, old)
+    def flat(self):
+        final = Counter({self.original_polymer[0]: 1, self.original_polymer[-1]: 1})
+        for (a, b), v in self.polymer.items():
+            final[a] += v
+            final[b] += v
+        final = Counter({k: v // 2 for k, v in final.items()})
+        return final
 
-    def do(self):
-        new_polymer = ''
-        for pair in zip(self.polymer, self.polymer[1:]):
-            new = multiple_replace(pair, self.parsed_data, 10)
-            new_polymer += new[:-1]
-        new_polymer += self.polymer[-1]
-        self.polymer = new_polymer
+    def count(self):
+        mc = self.flat().most_common()
+        return mc[0][1] - mc[-1][1]
 
     def part_a(self):
-        self.do()
-        c = Counter(self.polymer)
-        most, *_, least = c.most_common()
-        return most[1] - least[1]
+        for _ in range(10):
+            self.run()
+        return self.count()
 
     def part_b(self):
-        self.do()
-        self.do()
-        self.do()
-        c = Counter(self.polymer)
-        most, *_, least = c.most_common()
-        return most[1] - least[1]
+        for _ in range(30):
+            self.run()
+        return self.count()
 
--- tests/year_2021/day_14.py
+++ tests/year_2021/day_14.py
@@ -1,3 +1,5 @@
+from collections import defaultdict, Counter
+
 import pytest
 
 from aoc_cqkh42.year_2021 import day_14
@@ -28,15 +30,28 @@ CN -> C"""
 
 def test_part_a(solution):
     assert solution.part_a() == 1588
+    assert solution.flat().total() == 3073
+    assert solution.flat()['B'] == 1749
+    assert solution.flat()['C'] == 298
+    assert solution.flat()['H'] == 161
+    assert solution.flat()['N'] == 865
 
 
 def test_part_b(solution):
+    solution.part_a()
     assert solution.part_b() == 2188189693529
 
 
-def test_replace(solution):
-    assert day_14.replace('NNCB', solution.parsed_data) == 'NCNBCHB'
+def test_one_run(solution):
+    solution.run()
+    assert solution.polymer == defaultdict(int, {'NC': 1, 'CN': 1, 'NB': 1, 'BC': 1, 'CH': 1, 'HB': 1})
+    assert solution.count() == 1
 
 
-def test_multiple_replace(solution):
-    assert day_14.multiple_replace('NNCB', solution.parsed_data, 4) == 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'
+def test_four_run(solution):
+    solution.run()
+    solution.run()
+    solution.run()
+    solution.run()
+    assert solution.flat() == Counter({'B': 23, 'N': 11, 'C': 10, 'H': 5})
+    assert solution.polymer == defaultdict(int, {'NB': 9, 'BB': 9, 'BN': 6, 'BC': 4, 'CC': 2, 'CN': 3, 'NC': 1, 'CB': 5, 'BH': 3, 'HC': 3, 'HH': 1, 'HN': 1, 'NH': 1})
