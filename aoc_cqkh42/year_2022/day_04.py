2ec34d9b78d4a0e56ec13c105f7b676b2320d981
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-12-04 10:18:36 +0000

    2022 day 4

--- aoc_cqkh42/year_2022/day_04.py
+++ aoc_cqkh42/year_2022/day_04.py
@@ -1,9 +1,19 @@
 from aoc_cqkh42 import BaseSolution
 
+import parse
+
 
 class Solution(BaseSolution):
+    parser = parse.compile('{:d}-{:d},{:d}-{:d}')
+
+    def parse_data(self):
+        return [
+            (set(range(a, b+1)), set(range(c, d+1)))
+            for a, b, c, d in self.parser.findall(self.data)
+        ]
+
     def part_a(self):
-        ...
+        return sum(r.issubset(l) or l.issubset(r)for l, r in self.parsed_data)
 
     def part_b(self):
-        ...      
+        return sum(bool(r.intersection(l)) for l, r in self.parsed_data)
---
+++ tests/year_2022/day_04.py
@@ -0,0 +1,22 @@
+import pytest
+
+from aoc_cqkh42.year_2022 import day_04
+
+
+@pytest.fixture(scope='module')
+def solution():
+    data = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+    return day_04.Solution(data)
+
+
+def test_part_a(solution):
+    assert solution.part_a() == 2
+
+
+def test_part_b(solution):
+    assert solution.part_b() == float('inf')
