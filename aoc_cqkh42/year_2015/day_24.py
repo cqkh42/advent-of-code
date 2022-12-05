7bcd3b6c7336563809498360627d81350d4ffda7
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-12-05 20:58:36 +0000

    making 2015 pretty

--- aoc_cqkh42/year_2015/day_24.py
+++ aoc_cqkh42/year_2015/day_24.py
@@ -1,4 +1,3 @@
-# TODO really messy
 import itertools
 import math
 
@@ -10,40 +9,31 @@ from aoc_cqkh42 import BaseSolution
 def subsets_equal_to(set_, target):
     for size in range(1, len(set_)+1):
         combs = itertools.combinations(set_, size)
-        yield from (comb for comb in combs if sum(comb) == target)
+        for comb in combs:
+            if sum(comb) == target:
+                return comb
 
-def two_sets_sum_to(set_, target):
-    for first in subsets_equal_to(set_, target):
-        second = subsets_equal_to(set_.difference(first), target)
-        for s in second:
-            yield first, s
 
 def sets_sum_to(set_, target, num):
     if num == 1:
-        yield from subsets_equal_to(set_, target)
-    elif num == 2:
-        for first in subsets_equal_to(set_, target):
-            second = subsets_equal_to(set_.difference(first), target)
-            for s in second:
-                yield first, s
+        return [subsets_equal_to(set_, target)]
     else:
-        for results in sets_sum_to(set_, target, num-1):
-            next_ = subsets_equal_to(set_.difference(*results), target)
-            for t in next_:
-                yield *more_itertools.always_iterable(results), t
+        a = sets_sum_to(set_, target, num-1)
+        next_ = subsets_equal_to(set_.difference(a), target)
+        return *more_itertools.always_iterable(a), next_
+
 
 class Solution(BaseSolution):
     def parse_data(self):
         return frozenset(self.numbers)
 
-    def solve(self, num_groups, target):
-        results = next(sets_sum_to(self.parsed_data, target, num_groups))
-        groups = *results, self.parsed_data.difference(results)
+    def solve(self, num_groups):
+        groups = sets_sum_to(self.parsed_data, sum(self.parsed_data) / num_groups, num_groups)
         return min(math.prod(i) for i in groups if
                    len(i) == len(min(groups, key=len)))
 
     def part_a(self):
-        return self.solve(2, sum(self.parsed_data) / 3)
+        return self.solve(3)
 
     def part_b(self):
-        return self.solve(3, sum(self.parsed_data) / 4)
+        return self.solve(4)
--- tests/year_2015/test_day_17.py
+++ tests/year_2015/test_day_17.py
@@ -6,13 +6,13 @@ from aoc_cqkh42.year_2015 import day_17
 @pytest.fixture
 def solution():
     data = '20\n15\n10\n5\n5'
-    solution = day_17.Solution(data, 25)
+    solution = day_17.Solution(data)
     return solution
 
 
 def test_part_a(solution):
-    assert solution.part_a() == 4
+    assert solution.part_a(target=25) == 4
 
 
 def test_part_b(solution):
-    assert solution.part_b() == 3
+    assert solution.part_b(target=25) == 3
