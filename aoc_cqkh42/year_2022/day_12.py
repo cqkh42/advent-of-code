59b5f1e3bc00aa655ac863df31389ccb11fcf1d2
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-12-13 09:30:43 +0000

    2022 day 12

--- aoc_cqkh42/year_2022/day_12.py
+++ aoc_cqkh42/year_2022/day_12.py
@@ -3,10 +3,18 @@ from aoc_cqkh42 import BaseSolution
 import networkx as nx
 
 
+def manhattan(from_, to):
+    return abs(from_[0] - to[0]) + abs(from_[1] - to[1])
+
+
 class Solution(BaseSolution):
+    def is_valid_edge(self, u, v):
+        f = self.lines[u[1]][u[0]]
+
     def parse_data(self):
-        graph = nx.grid_2d_graph(len(self.lines[0]), len(self.lines),
-                                 create_using=nx.DiGraph)
+        graph = nx.grid_2d_graph(
+            len(self.lines[0]), len(self.lines), create_using=nx.DiGraph
+        )
         to_remove = []
         for (from_x, from_y), (to_x, to_y) in graph.edges(data=False):
             f = self.lines[from_y][from_x]
@@ -30,10 +38,10 @@ class Solution(BaseSolution):
                     start = (col_index, row_index)
                 if val == 'E':
                     end = (col_index, row_index)
-        return nx.shortest_path_length(self.parsed_data, start, end)
+        self.answer_a = nx.shortest_path_length(self.parsed_data, start, end)
+        return self.answer_a
 
     def part_b(self):
-        reversed_graph = self.parsed_data.reverse()
         to_search = []
         for row_index, row in enumerate(self.lines):
             for col_index, val in enumerate(row):
@@ -41,12 +49,6 @@ class Solution(BaseSolution):
                     to_search.append((col_index, row_index))
                 if val == 'E':
                     end = (col_index, row_index)
-        paths = []
-        for start in to_search:
-            try:
-                length = nx.shortest_path_length(self.parsed_data, start, end)
-            except nx.NetworkXNoPath:
-                continue
-            else:
-                paths.append(length)
-        return min(paths)
+        a = nx.single_target_shortest_path_length(self.parsed_data, end, self.answer_a)
+        a = dict(a)
+        return min(a.get(i, float('inf')) for i in to_search)
