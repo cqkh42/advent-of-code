2fba983080ac681a6107fe17f6324e2ef7746961
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-12-13 11:01:14 +0000

    2022 day 5 all pretty

--- aoc_cqkh42/year_2022/day_05.py
+++ aoc_cqkh42/year_2022/day_05.py
@@ -1,32 +1,40 @@
-from collections import defaultdict
 from copy import deepcopy
-
-from aoc_cqkh42 import BaseSolution
 
 import more_itertools
 import parse
+
+from aoc_cqkh42 import BaseSolution
 
 
 class Solution(BaseSolution):
     parser = parse.compile('move {:d} from {:d} to {:d}')
 
+    @property
+    def instructions(self):
+        for count, from_, to in self.parser.findall(self.data):
+            yield count, from_ - 1, to - 1
+
     def parse_data(self):
-        a, _ = self.data.split('\n\n')
-        queues = [[x for x in reversed(i) if x != ' '] for i in zip(*a.split('\n')) if i[-1] != ' ']
-        queues = [[], *queues]
+        lines, _ = more_itertools.split_at(self.lines, lambda line: line == '')
+        stacks = (stack for stack in zip(*lines) if stack[-1] != ' ')
+        queues = [
+            [box for box in stack if box != ' '][::-1]
+            for stack in stacks
+        ]
         return queues
 
-    def part_a(self):
+    def move_boxes(self, lifo=True):
         queues = deepcopy(self.parsed_data)
-        for count, from_, to in self.parser.findall(self.data):
-            p = queues[from_][-count:]
-            queues[to].extend(reversed(p))
+        for count, from_, to in self.instructions:
+            if lifo:
+                queues[to].extend(reversed(queues[from_][-count:]))
+            else:
+                queues[to].extend(queues[from_][-count:])
             del queues[from_][-count:]
-        return ''.join(i[-1] for i in queues[1:])
+        return ''.join(i[-1] for i in queues)
+
+    def part_a(self):
+        return self.move_boxes()
 
     def part_b(self):
-        queues = deepcopy(self.parsed_data)
-        for count, from_, to in self.parser.findall(self.data):
-            queues[to].extend(queues[from_][-count:])
-            del queues[from_][-count:]
-        return ''.join(i[-1] for i in queues[1:])
+        return self.move_boxes(False)
