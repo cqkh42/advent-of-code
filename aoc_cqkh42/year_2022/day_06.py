bce993a63a4f20dc6c1f080f0d4984a8fbdf3381
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-12-13 11:17:28 +0000

    2022 day 6 all pretty

--- aoc_cqkh42/year_2022/day_06.py
+++ aoc_cqkh42/year_2022/day_06.py
@@ -1,13 +1,15 @@
-from aoc_cqkh42 import BaseSolution
-
 import more_itertools
+
+from aoc_cqkh42 import BaseSolution
 
 
 class Solution(BaseSolution):
     def solve(self, size):
-        for index in range(len(self.data)):
-            if more_itertools.all_unique(self.data[index:index+size]):
-                return index+size
+        is_unique = more_itertools.first_true(
+            enumerate(more_itertools.windowed(self.data, size), start=size),
+            pred=lambda x: more_itertools.all_unique(x[1])
+        )
+        return is_unique[0]
 
     def part_a(self):
         return self.solve(4)
