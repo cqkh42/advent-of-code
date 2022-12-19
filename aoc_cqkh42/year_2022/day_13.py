dfd728b5f439c090acf2a26d554198bbad314fad
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-12-19 13:29:59 +0000

    2022 day 13 part b

--- aoc_cqkh42/year_2022/day_13.py
+++ aoc_cqkh42/year_2022/day_13.py
@@ -3,12 +3,23 @@ from aoc_cqkh42 import BaseSolution
 from dataclasses import dataclass
 
 import json
+import math
+import more_itertools
 
 
 @dataclass
 class Packet:
-    def __init__(self, string):
-        self.packet = json.loads(string)
+    packet: str
+
+    def __post_init__(self):
+        self.packet = json.loads(self.packet)
+
+    def __lt__(self, other):
+        return compare(self.packet, other.packet)
+
+    def __eq__(self, other):
+        if isinstance(other, list):
+            return self.packet == other
 
 
 def compare(left, right):
@@ -17,31 +28,41 @@ def compare(left, right):
             return None
         return left < right
     elif isinstance(left, int) and isinstance(right, list):
-        left = [left]
-        return compare(left, right)
+        return compare([left], right)
     elif isinstance(left, list) and isinstance(right, int):
-        right = [right]
-        return compare(left, right)
-
+        return compare(left, [right])
     elif isinstance(left, list) and isinstance(right, list):
         for a, b in zip(left, right):
-            if compare(a, b) is not None:
-                return compare(a, b)
+            result = compare(a, b)
+            if result is not None:
+                return result
         if len(left) == len(right):
             return None
         return len(left) < len(right)
 
 
 class Solution(BaseSolution):
+    def parse_data(self):
+        pairs = self.data.split('\n\n')
+        pairs = (pair.split('\n') for pair in pairs)
+        pairs = [(Packet(left), Packet(right)) for left, right in pairs]
+        return pairs
+
     def part_a(self):
-        total = 0
-        for index, pair in enumerate(self.data.split('\n\n'), start=1):
-            a, b = pair.split('\n')
-            a = list(json.loads(a))
-            b = list(json.loads(b))
-            result = compare(a, b)
-            total += result * index
-        return total
+        return sum(
+            index
+            for index, (left, right) in enumerate(self.parsed_data, start=1)
+            if left < right
+        )
 
     def part_b(self):
-        ...
+        packets = sorted((
+            *more_itertools.flatten(self.parsed_data),
+            Packet('[[2]]'),
+            Packet('[[6]]')
+        ))
+        return math.prod(
+            index
+            for index, packet in enumerate(packets, start=1)
+            if packet in ([[2]], [[6]])
+        )
