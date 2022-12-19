c893422d7034bde458189ec01a12b8e27b7c1c6b
Jack Cooper-McEwan <jackcooper93@gmail.com>
2022-12-19 21:02:11 +0000

    2022 day 15 part a

--- aoc_cqkh42/year_2022/day_15.py
+++ aoc_cqkh42/year_2022/day_15.py
@@ -1,9 +1,57 @@
 from aoc_cqkh42 import BaseSolution
 
+# 6228183 is too high
+
+import parse
+
+
+def manhattan(u, v):
+    return abs(u[0] - v[0]) + abs(u[1] - v[1])
+
+
+def resolve_manhattan(u, distance, known_y):
+    partial_diff = abs(u[1] - known_y)
+    needed = distance - partial_diff
+    if needed < 0:
+        raise ValueError
+    return sorted((u[0] - needed, u[0] + needed))
+
 
 class Solution(BaseSolution):
-    def part_a(self):
-        ...
+    parser = parse.compile(
+        'Sensor at x={:d}, y={:d}: closest beacon is at x={:d}, y={:d}'
+    )
+    beacons = set()
 
-    def part_b(self):
+    def parse_data(self):
+        sensors = {}
+        for sensor_x, sensor_y, beacon_x, beacon_y in self.parser.findall(self.data):
+            sensors[(sensor_x, sensor_y)] = manhattan((sensor_x, sensor_y), (beacon_x, beacon_y))
+            self.beacons.add((beacon_x, beacon_y))
+        # print(sensors)
+        return sensors
+
+    def part_a(self, y=2_000_000):
+        unavailable = set()
+        for sensor, distance in self.parsed_data.items():
+            try:
+                start, end = resolve_manhattan(sensor, distance, y)
+                # print(sensor, start, end, distance)
+            except ValueError:
+                # print('raised')
+                continue
+            else:
+                for num in range(start, end+1):
+                    unavailable.add(num)
+        for beacon in self.beacons:
+            if beacon[1] == y:
+                unavailable.remove(beacon[0])
+        for sensor in self.parsed_data:
+            if sensor[1] == y:
+                unavailable.remove(sensor[0])
+                # print()
+        # print(sorted(unavailable))
+        return len(unavailable)
+
+    def part_b(self, limit=4_000_000):
         ...
---
+++ tests/year_2022/test_day_15.py
@@ -0,0 +1,42 @@
+import pytest
+
+from aoc_cqkh42.year_2022 import day_15
+
+
+@pytest.fixture(scope='module')
+def solution():
+    data = """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
+    return day_15.Solution(data)
+
+
+def test_resolve_manhattan():
+    assert day_15.resolve_manhattan((8, 7), 9, 10) == [2, 14]
+    with pytest.raises(ValueError):
+        day_15.resolve_manhattan((0,0), 5, 10)
+
+    with pytest.raises(ValueError):
+        day_15.resolve_manhattan((14, 3), 1, 11)
+    assert day_15.resolve_manhattan((391282,2038170), 1052098, 2_000_000) == [-622646, 1405210]
+
+
+def test_part_a(solution):
+    assert solution.part_a(9) == 25
+    assert solution.part_a(10) == 26
+    assert solution.part_a(11) == 27
+
+
+def test_part_b(solution):
+    assert solution.part_b(20) == 56000011
