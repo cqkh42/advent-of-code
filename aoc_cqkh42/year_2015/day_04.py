2e36274abac7689c482dc5bd71350b9e1bb4a9f8
Jack Cooper-McEwan <jackcooper93@gmail.com>
2023-05-31 22:13:30 +0100

    2019 day 4

--- aoc_cqkh42/year_2015/day_04.py
+++ aoc_cqkh42/year_2015/day_04.py
@@ -2,25 +2,38 @@
 import itertools
 from _md5 import md5
 
+from aocd import get_data, submit
+
 from aoc_cqkh42 import BaseSolution
 
 
 class Solution(BaseSolution):
-    counter = 0
+    counter = itertools.count()
 
     def _crack_hash(self, sequence):
-        for answer in itertools.count(self.counter):
-            hashed = self.parsed_data.copy()
-            hashed.update(f"{answer}".encode())
-            hashed = hashed.hexdigest()
-            if hashed.startswith(sequence):
+        for answer in self.counter:
+            hash_ = self.parsed_data.copy()
+            hash_.update(f"{answer}".encode())
+            if hash_.hexdigest().startswith(sequence):
                 return answer
 
     def parse_data(self):
         return md5(self.data.encode())
 
     def part_a(self):
-        return self._crack_hash("0" * 5)
+        answer = self._crack_hash("0" * 5)
+        return answer
 
     def part_b(self):
         return self._crack_hash("0" * 6)
+
+
+if __name__ == "__main__":
+    data = get_data(
+        session='53616c7465645f5f7578abb37529958ea6c4839cbbeeb414870a119609f21ea219dd7295504325ddb3aa309f17baa02e1a0c67d897449610aa0d811e4d721457',
+        day=4, year=2015)
+    solution = Solution(data)
+    submit(solution.part_a(), part='a', day=4, year=2015,
+           session='53616c7465645f5f7578abb37529958ea6c4839cbbeeb414870a119609f21ea219dd7295504325ddb3aa309f17baa02e1a0c67d897449610aa0d811e4d721457')
+    submit(solution.part_b(), part='b', day=4, year=2015,
+           session='53616c7465645f5f7578abb37529958ea6c4839cbbeeb414870a119609f21ea219dd7295504325ddb3aa309f17baa02e1a0c67d897449610aa0d811e4d721457')
--- tests/year_2015/test_day_04.py
+++ tests/year_2015/test_day_04.py
@@ -3,7 +3,8 @@ import pytest
 from aoc_cqkh42.year_2015 import day_04
 
 
-@pytest.mark.parametrize("data, answer", [("abcdef", 609043), ("pqrstuv", 1048970)])
+@pytest.mark.parametrize("data, answer",
+                         [("abcdef", 609_043), ("pqrstuv", 1_048_970)])
 def test_part_a(data, answer):
     solution = day_04.Solution(data)
     assert solution.part_a() == answer
