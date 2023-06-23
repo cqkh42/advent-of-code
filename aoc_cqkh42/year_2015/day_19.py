472c768ca5b7b038dfc4fde986c3634f1fa2bfbe
Jack Cooper-McEwan <jackcooper93@gmail.com>
2023-06-23 16:53:05 +0100

    cyk!

--- aoc_cqkh42/year_2015/day_19.py
+++ aoc_cqkh42/year_2015/day_19.py
@@ -1,43 +1,19 @@
-# TODO chompsky language decomposition
+import collections
+import itertools
+import re
+from functools import cached_property
+
+from multidict import MultiDict
 
 from aoc_cqkh42 import BaseSolution
 from .day_19_help import input_
 
 
-def replace_occurence(string, old, new, occurence):
-    return (
-        string.replace(old, "$$$", occurence - 1)
-        .replace(old, new, 1)
-        .replace("$$$", old)
-    )
-
-
-import itertools
-import collections
-import re
-from functools import cached_property
-
-
 class NewCYKRunner:
-    def __init__(self, maps, molecule):
-        calibrate = set()
-        rules = collections.defaultdict(list)
-        revrules = collections.defaultdict(list)
-
-        pattern = r"(.+) => (.+)"
-        for src, repl in re.findall(pattern, maps):
-            rules[src] += [repl]
-            revrules[repl] += [src]
-
-        for src, repl in re.findall(pattern, maps):
-            for start in re.finditer(src, ''.join(molecule)):
-                start = start.start()
-                calibrate.add(''.join(molecule[:start]) + repl + ''.join(
-                    molecule[start + len(src):]))
-        self.molecule = molecule
+    def __init__(self, rules, molecule):
         self.rules = rules
-        self.revrules = revrules
-        self.calibrate = set()
+
+        self.molecule = molecule
         self.cyk = collections.defaultdict(
             lambda: collections.defaultdict(list))
         self.backref = collections.defaultdict(
@@ -45,20 +21,22 @@ class NewCYKRunner:
         self.counter = 0
 
     @cached_property
-    def letters(self):
-        return len(self.molecule) - 1
+    def elements(self):
+        return sum(char.isupper() for char in self.molecule) - 1
 
     def run(self):
-        for index, element in enumerate(self.molecule):
+        for index, element in enumerate(
+                re.findall(r'[A-Z][a-z]?', self.molecule)):
             self.cyk[0][index] = [element]
-        for y in range(1, self.letters + 1):
-            for x in range(self.letters - y + 1):
+
+        for y in range(1, self.elements + 1):
+            for x in range(self.elements - y + 1):
                 for i in range(y):
                     need = [t[0] + t[1] for t in
                             itertools.product(self.cyk[i][x],
                                               self.cyk[y - i - 1][x + i + 1])]
                     for n in need:
-                        for blah in self.revrules.get("".join(n), []):
+                        for blah in self.rules.getall("".join(n), []):
                             if blah not in self.cyk[y][x]:
                                 self.cyk[y][x].append(blah)
                                 self.backref[y][x].append(
@@ -74,30 +52,27 @@ class NewCYKRunner:
 
 
 class Solution(BaseSolution):
-    element = None
-    maps = None
     new_maps = input_
+    molecule = None
+    rules = None
 
     def parse_data(self):
-        *maps, _, element = self.lines
-        maps = [item.split(" => ") for item in maps]
-        self.element = element
-        self.maps = maps
-        return maps, element
+        self.molecule = self.lines[-1]
+        self.rules = MultiDict(
+            ((v, k) for k, v in re.findall(r"(.+) => (.+)", input_)))
 
     def part_a(self):
         new_strings = set()
-        for old, new in self.maps:
-            count = self.element.count(old)
-            for occurence in range(1, count + 1):
-                new_string = replace_occurence(self.element, old, new,
-                                               occurence)
-                new_strings.add(new_string)
+        for old, new in re.findall(r"(.+) => (.+)", self.data):
+            replacements = {
+                self.molecule[:i.start()] + new + self.molecule[i.end():] for i
+                in re.finditer(old, self.molecule)}
+
+            new_strings.update(replacements)
         return len(new_strings)
 
     def part_b(self):
-        p = re.findall('[A-Z][a-z]?', self.element)
-        solution = NewCYKRunner(input_, p)
+        solution = NewCYKRunner(self.rules, self.molecule)
         solution.run()
-        solution.recur(0, solution.letters)
+        solution.recur(0, solution.elements)
         return solution.counter
