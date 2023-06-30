d73346699afa88e2b0d60549d10c95debfac42b2
Jack Cooper-McEwan <jackcooper93@gmail.com>
2023-06-30 23:11:55 +0100

    formatting changes

--- aoc_cqkh42/year_2015/day_10.py
+++ aoc_cqkh42/year_2015/day_10.py
@@ -1,6 +1,5 @@
 import itertools
 from functools import cache
-from typing import Self
 
 from aoc_cqkh42.helpers.base_solution import BaseSolution
 
@@ -14,18 +13,15 @@ class Solution(BaseSolution):
     def _iteration(self):
         g = itertools.groupby(self.input_)
         d = (f"{len(list(b))}{a}" for a, b in g)
-        self.data = "".join(d)
+        self.input_ = "".join(d)
 
     def _sequence(self, iters):
         for _ in range(iters):
             self._iteration()
-        return len(self.data)
+        return len(self.input_)
 
     def part_a(self, iters=40):
         return self._sequence(iters)
 
     def part_b(self):
         return self._sequence(10)
-
-    def _process_data(self: Self) -> str:
-        return self.data
