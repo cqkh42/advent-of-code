564e2a443850fb447d522a743c771e7663cca6de
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-09-10 20:43:34 +0100

    half working

--- aoc_cqkh42/year_2016/day_22.py
+++ aoc_cqkh42/year_2016/day_22.py
@@ -1,8 +1,9 @@
-import functools
+from functools import cached_property
 import itertools
 from dataclasses import dataclass, field, replace
 
 import more_itertools
+from frozendict import frozendict
 import parse
 
 from aoc_cqkh42.helpers.base_solution import BaseSolution
@@ -17,8 +18,6 @@ PARSER = parse.compile(
 
 @dataclass(frozen=True)
 class Cell:
-    x: int
-    y: int
     size: int
     used: int
 
@@ -30,89 +29,98 @@ class Cell:
         return replace(self, used=self.used+volume)
 
 
-@functools.cache
-def is_cell_valid(cell: Cell):
-    return cell.x > 0 and cell.y > 0 and cell.avail > 0
-
 @dataclass(frozen=True)
 class Grid(AStarBaseNode):
     current_location: tuple
-    grid: set = field(default_factory=lambda: frozenset())
+    empty_location: tuple
+    grid: frozendict = field(default_factory=lambda: frozendict())
     distance: int = field(default=0, hash=False, compare=False)
+    h: int = field(default=0)
 
     def __iter__(self):
         yield from self.grid
 
     def __getitem__(self, item):
-        x, y = item
-        return more_itertools.only(
-            cell for cell in self if cell.x == x and cell.y == y
-        )
-
-    def __len__(self):
-        return len(self.grid)
-
-    @property
-    def h(self):
-        return sum(self.current_location)
+        return self.grid.get(item)
 
     def is_valid(self):
-        return all(is_cell_valid(cell) for cell in self)
+        return True
 
-    def move_data(self, origin: Cell, target: Cell):
-        volume = origin.used
-        new_origin = origin.change_data(-volume)
-        new_target = target.change_data(volume)
-        new_grid = set(self.grid.difference({origin, target}))
-        new_grid.add(new_origin)
-        new_grid.add(new_target)
-        return replace(self, grid=frozenset(new_grid), distance=self.distance+1)
+    def move_data(self, origin: tuple, target: tuple):
+        origin_cell = self[origin]
+        volume = origin_cell.used
+        new_origin = origin_cell.change_data(-volume)
+        new_target = self[target].change_data(volume)
+        new_grid = self.grid.delete(origin).delete(target).set(origin, new_origin).set(target, new_target)
+
+        d = abs(origin[0] - self.current_location[0]) + abs(origin[1] - self.current_location[1])
+        return replace(self, grid=new_grid, distance=self.distance+1, h=d)
 
     def is_target(self):
         return self.current_location == (0, 0)
 
     def neighbours(self):
         iterations = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        for cell in self:
-            for x, y in iterations:
-                target_x = cell.x + x
-                target_y = cell.y + y
-                target_cell = self[target_x, target_y]
-                # except IndexError:
-                #     continue
-                if not target_cell:
-                    continue
-                if target_cell.avail >= cell.used:
-                    moved = self.move_data(cell, target_cell)
-                    if (cell.x, cell.y) == self.current_location:
-                        new_current_location = (target_x, target_y)
-                        yield replace(moved, current_location=new_current_location)
-                    else:
-                        yield moved
+        # c = (
+        #     (self.empty, self.empty[0])
+        # )
+        c = (
+            ((self_x, self_y), (self_x + x, self_y + y))
+            for ((self_x, self_y), (x, y))
+            in itertools.product(self, iterations)
+        )
+        c = ((self_, target) for self_, target in c if self[target])
+        c = ((self_, target) for self_, target in c if self[target].avail >= self[self_].used)
+        for self_, target in c:
+            moved = self.move_data(self_, target)
+            if self_ == self.current_location:
+                new_current_location = target
+                yield replace(moved, current_location=new_current_location)
+            else:
+                yield moved
 
 
 class Solution(BaseSolution):
     def _process_data(self):
         rows = PARSER.findall(self.input_)
-        cells = frozenset({Cell(**row.named['test']) for row in rows})
-        max_x = max(cell.x for cell in cells)
+        cells = [
+            (row.named['test']) for row in rows
+        ]
+        cells = frozendict({
+            (cell['x'], cell['y']): Cell(cell['size'], cell['used']) for cell in cells
+        })
+        max_x = max(x for (x, y) in cells)
 
-        nodes = Grid((max_x, 0), cells)
+        empty = [k for k, v in cells.items() if v.used == 0][0]
+
+        nodes = Grid((max_x, 0), empty, cells, 0)
         return nodes
 
+    @cached_property
+    def possible_combinations(self):
+        return [
+            (node_a, node_b)
+            for node_a, node_b in itertools.combinations(self.processed.grid, 2) if
+            (self.processed.grid[node_a].used and self.processed.grid[node_a].used <= self.processed.grid[node_b].avail) or
+            (self.processed.grid[node_b].used and self.processed.grid[node_b].used <= self.processed.grid[node_a].avail)
+        ]
+
     def part_a(self):
-        viable = (
-            (node_a.used and node_a.used <= node_b.avail) or
-            (node_b.used and node_b.used <= node_a.avail)
-            for node_a, node_b in itertools.combinations(self.processed, 2)
-        )
-        return sum(viable)
+        return len(self.possible_combinations)
 
     def part_b(self):
-        solver = AStar(self.processed).run()
-        return solver
+        # raise
+        in_game = set(more_itertools.flatten(a for a in self.possible_combinations))
+        in_game = frozendict({i: self.processed.grid[i] for i in in_game})
+        self.processed = replace(self.processed, grid=in_game)
+        # self.processed.grid = in_game
+        # return
+        solver = AStar(self.processed)
+        answer = solver.run()
+        print(len(solver.visited))
+        return answer
 
 
 if __name__ == "__main__":
     # started at 21:40
-    submit_answers(Solution, 22, 2016)
+    s = submit_answers(Solution, 22, 2016)
