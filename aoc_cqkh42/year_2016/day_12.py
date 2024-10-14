349ea3ba65c9317a5191fe76cffb5709a9295587
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-10-14 09:16:27 +0100

    day 23 part 1

--- aoc_cqkh42/year_2016/day_12.py
+++ aoc_cqkh42/year_2016/day_12.py
@@ -1,4 +1,6 @@
 from aoc_cqkh42.helpers.base_solution import BaseSolution
+from aoc_cqkh42 import submit_answers
+
 
 
 class Solution(BaseSolution):
@@ -19,8 +21,31 @@ class Computer:
         self.instructions = [
             instruction.split() for instruction in instructions.split('\n')
         ]
+        # for i in self.instructions:
+        #     print(i)
+        # print()
+        self.compile_instructions()
+        # for i in self.instructions:
+        #     print(i)
+        # raise
         self.registers = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
         self.index = 0
+
+    def compile_instructions(self):
+        for index in range(len(self.instructions)):
+            instructions = self.instructions[index:index+3]
+            commands = [i[0] for i in instructions]
+            if commands == ['inc', 'dec', 'jnz']:
+                from_ = instructions[1][1]
+                to_ = instructions[0][1]
+                new_instruction = ['mul', from_, to_]
+                self.instructions[index] = new_instruction
+            elif commands == ['dec', 'inc', 'jnz']:
+                from_ = instructions[0][1]
+                to_ = instructions[1][1]
+                new_instruction = ['mul', from_, to_]
+                self.instructions[index] = new_instruction
+
 
     def run_step(self):
         cmd, input_1, *values = self.instructions[self.index]
@@ -37,6 +62,16 @@ class Computer:
             val_2 = self.registers.get(values[0], values[0])
             if int(val_1):
                 incr = int(val_2)
+        elif cmd == 'mul':
+            from_ = self.registers.get(input_1, input_1)
+            self.registers[values[0]] += int(from_)
+            self.registers[input_1] = 0
+            incr=3
+        elif cmd == 'inv_mul':
+            from_ = self.registers.get(values[0], values[0])
+            self.registers[input_1] += int(from_)
+            self.registers[values[0]] = 0
+            incr = 3
         elif cmd == 'tgl':
             val = self.registers.get(input_1, input_1)
             focus = self.index + val
@@ -45,6 +80,9 @@ class Computer:
                     self.instructions[focus])
             except IndexError:
                 pass
+
+        else:
+            raise
         self.index += incr
 
     def run(self):
@@ -62,6 +100,12 @@ def process_toggle(instruction):
         'dec': 'inc',
         'jnz': 'cpy',
         'cpy': 'jnz',
-        'tgl': 'inc'
+        'tgl': 'inc',
+        'mul': 'inv_mul',
+        'inv_mul': 'mul'
     }
     return mapping[cmd], input_1, *values
+
+
+if __name__ == "__main__":
+    submit_answers(Solution, 12, 2016)
