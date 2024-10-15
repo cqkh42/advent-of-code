46aadf41e7dfc84d24ea1573ed3adf2fb8df7a0b
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-10-15 05:51:46 +0100

    day 23

--- tests/year_2016/test_day_23.py
+++ tests/year_2016/test_day_23.py
@@ -18,17 +18,3 @@ def test_computer(data):
     c = Computer(data)
     c.run()
     assert c.registers['a'] == 3
-
-
-def test_mul():
-    data = """cpy b c
-    inc a
-    dec c
-    jnz c -2
-    dec d
-    jnz d -5"""
-    c = Computer(data)
-    c.registers['b'] = 6
-    c.registers['d'] = 5
-    c.run()
-    print(c.registers)
