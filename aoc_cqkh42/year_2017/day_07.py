a2f85ace9aa0fef7a480c29b75e261332d2b3b97
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-10-15 15:49:55 +0100

    reformatting 2017

--- aoc_cqkh42/year_2017/day_07.py
+++ aoc_cqkh42/year_2017/day_07.py
@@ -1,14 +1,33 @@
 import itertools
 
+import functools
+import itertools
+from dataclasses import dataclass
 
-def part_a(data):
-    holding = [row for row in data.split('\n') if '->' in row]
-    lefts = {row.split()[0] for row in holding}
-    rights = (row.split('-> ')[1] for row in holding)
-    rights = [row.split(', ') for row in rights]
-    rights = set(itertools.chain.from_iterable(rights))
-    return lefts.difference(rights).pop()
+@dataclass
+class Program:
+    name: str
+    weight: int
+    dependencies: list[str]
+
+from aoc_cqkh42.helpers.base_solution import BaseSolution
+from aoc_cqkh42 import submit_answers
+
+class Solution(BaseSolution):
+
+    def part_a(self):
+
+        holding = [row for row in self.input_.split('\n') if '->' in row]
+        lefts = {row.split()[0] for row in holding}
+        rights = (row.split('-> ')[1] for row in holding)
+        rights = [row.split(', ') for row in rights]
+        rights = set(itertools.chain.from_iterable(rights))
+        return lefts.difference(rights).pop()
 
 
-def part_b(data, **_):
-    return False
+    def part_b(data, **_):
+        return False
+
+
+if __name__ == "__main__":
+    submit_answers(Solution, 7, 2017)
