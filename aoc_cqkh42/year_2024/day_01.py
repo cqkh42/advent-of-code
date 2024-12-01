98dd32075fe4a17fd63677d3d3b3409f44efe4f3
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-12-01 10:35:41 +0000

    2024 day 1

---
+++ aoc_cqkh42/year_2024/day_01.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from typing import Self
+
+from aoc_cqkh42 import submit_answers
+from aoc_cqkh42.helpers.base_solution import BaseSolution
+
+class Solution(BaseSolution):
+    def _parse(self: Self) -> tuple[list[int, ...], list[int, ...]]:
+        return sorted(self.numbers[::2]), sorted(self.numbers[1::2])
+
+    def part_a(self):
+        c = [abs(left - right) for left, right in zip(*self.parsed)]
+        return sum(c)
+
+    def part_b(self):
+        counted = Counter(self.parsed[1])
+
+        return sum(left*counted[left] for left in self.parsed[0])
+
+if __name__ == "__main__":
+    submit_answers(Solution, 1, 2024)
