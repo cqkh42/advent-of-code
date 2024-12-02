4cde13a4762581bf2425e6c258384f78ad5e5409
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-12-02 10:20:51 +0000

    2024 day 2

---
+++ aoc_cqkh42/year_2024/day_02.py
@@ -0,0 +1,39 @@
+import more_itertools
+import parse
+
+from aoc_cqkh42 import submit_answers
+from aoc_cqkh42.helpers.base_solution import BaseSolution
+
+def remove_list_items(line):
+    for index in range(len(line)):
+        yield line[:index] + line[index+1:]
+
+
+class Solution(BaseSolution):
+    PARSER = parse.compile('{:d}')
+
+    def _parse_line(self, line: str):
+        return [result[0] for result in self.PARSER.findall(line)]
+
+    def is_safe_line(self, line):
+        line = list(more_itertools.pairwise(line))
+        decreasing = all(x > y for x, y in line)
+        increasing = all(y > x for x, y in line)
+        in_range = all(abs(x - y) <= 3 for x, y in line)
+        return bool(in_range and (decreasing or increasing))
+
+    def part_a(self):
+        return sum(self.is_safe_line(line) for line in self.parsed_lines)
+
+    def could_be_safe_line(self, line):
+        removals = (
+            self.is_safe_line(option) for option in remove_list_items(line)
+        )
+        return self.is_safe_line(line) or any(removals)
+
+    def part_b(self):
+        return sum(self.could_be_safe_line(line) for line in self.parsed_lines)
+
+
+if __name__ == "__main__":
+    submit_answers(Solution,2, 2024)
