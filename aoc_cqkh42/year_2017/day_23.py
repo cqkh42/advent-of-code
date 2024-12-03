2f10976459bec2cfa3351507bc924c71daef61d2
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-12-03 20:33:30 +0000

    2017 day 23

--- aoc_cqkh42/year_2017/day_23.py
+++ aoc_cqkh42/year_2017/day_23.py
@@ -1,7 +1,16 @@
 from operator import mul, sub
 
+import pandas as pd
+
 from aoc_cqkh42 import submit_answers
 from aoc_cqkh42.helpers.base_solution import BaseSolution
+
+import math
+
+def is_prime(n):
+    if n % 2 == 0 and n > 2:
+        return False
+    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
 
 func_map = {
     "mul": mul,
@@ -25,10 +34,16 @@ class Program:
     def __setitem__(self, key, value):
         self.registers[key] = value
 
-    def run_line(self):
+    def run_line(self, optimised=False):
         command, x,y = self.instructions[self.index]
         self.history.append(command)
         step = 1
+        if self.index == 11 and optimised:
+            print(self.registers)
+            # step = 12
+            # self['g'] = 0
+            # self['f'] = 0
+            self.history.append('mul')
         if command in {"mul", "sub", "set"}:
             self[x] = func_map[command](self[x], self[y])
         elif command == "jnz" and self[x] != 0:
@@ -48,21 +63,15 @@ class Solution(BaseSolution):
             except IndexError:
                 return computer.history.count('mul')
 
-    def optimise_program(self):
-        a = list(self.parsed_lines)
-
-
-
     def part_b(self):
         computer = Program(self.parsed_lines)
         computer['a'] = 1
-        for i in range(10_000_000):
-            try:
-                print(computer.index)
-                computer.run_line()
-            except IndexError:
-                return computer['h']
-
+        for _ in range(7):
+            computer.run_line()
+        return sum(
+            not is_prime(num)
+            for num in range(computer['b'],computer['c'] +1, 17)
+        )
 
 if __name__ == "__main__":
     submit_answers(Solution,23, 2017)
