adad62caae5bf1315b9fdff16047bcb32940b810
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-12-11 11:23:44 +0000

    2024 day 09 - part b

--- aoc_cqkh42/year_2024/day_09.py
+++ aoc_cqkh42/year_2024/day_09.py
@@ -1,6 +1,6 @@
 from typing import Self, Any
 
-import more_itertools
+from more_itertools import first, iter_index, flatten
 
 from aoc_cqkh42 import submit_answers
 from aoc_cqkh42.helpers.base_solution import BaseSolution
@@ -31,21 +31,40 @@ class Solution(BaseSolution):
         return sum(index*num for index, num in enumerate(new_field))
 
     def part_b(self):
-        return 1
         field = []
+        # for index, num in enumerate('2333133121414131402'):
         for index, num in enumerate(self.input_):
             if index % 2:
-                field.append((int(num), int(num)))# we're on a storage block
+                field.append([None]*int(num))# we're on a storage block
             else:
-                field.append((index // 2, 0))
-        return field
+                field.append([index // 2]*int(num))
 
+        moved = set()
 
+        for mover in field[::-1][:-1]:
+            to_move = [i for i in mover if i is not None]
 
+            if not to_move:
+                continue
+            if to_move[0] in moved:
+                continue
+            moved.add(to_move[0])
+            size = len(to_move)
+            try:
+                f = first(iter_index((len(block) >= size and set(block) == {None} for block in field), True))
+            except ValueError:
+                pass
+            else:
+                z = field.index(mover)
+                if f >= z:
+                    continue
+                for _ in range(size):
+                    field[f].remove(None)
+                field.remove(mover)
+                field.insert(f, to_move)
+                field.insert(z, [None]*size)
 
-
-    def part_b(self):
-        ...
+        return sum(index * num for index, num in enumerate(flatten(field)) if num)
 
 
 if __name__ == "__main__":
