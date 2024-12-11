4074c7e89b06b3de81e9b25082abe122acf49c2d
Jack Cooper-McEwan <jackcooper93@gmail.com>
2024-12-11 11:45:21 +0000

    2024 day 10. finally easy

--- aoc_cqkh42/year_2024/day_10.py
+++ aoc_cqkh42/year_2024/day_10.py
@@ -1,6 +1,7 @@
-import itertools
+from itertools import product
 from typing import Self, Any
 
+import more_itertools
 import networkx as nx
 
 from aoc_cqkh42 import submit_answers
@@ -9,43 +10,39 @@ from aoc_cqkh42.helpers.base_solution import BaseSolution
 
 class Solution(BaseSolution):
     heights = None
+    zeros = None
+
+    def grid(self):
+        for y_index, line in enumerate(self.lines):
+            for x_index, value in enumerate(line):
+                yield (x_index, y_index), int(value)
+
     def _parse(self: Self) -> Any:
         g = nx.DiGraph()
         heights = {}
-        for y_index, line in enumerate(self.lines):
-            for x_index, value in enumerate(line):
-                heights[(x_index, y_index)] = int(value)
+        for (x, y), value in self.grid():
+                heights[(x, y)] = value
                 for x_delta, y_delta in (0,1), (0,-1), (1, 0), (-1, 0):
                     try:
-                        other_node = self.lines[y_index+y_delta][x_index+x_delta]
+                        other_node = self.lines[y+y_delta][x+x_delta]
 
-                        if int(value) + 1 == int(other_node):
-                            g.add_edge((x_index, y_index), (x_index+x_delta, y_index+y_delta))
+                        if value + 1 == int(other_node):
+                            g.add_edge((x, y), (x+x_delta, y+y_delta))
                     except IndexError:
                         continue
         self.heights = heights
-        return g
+
+        zeros = [k for k in self.heights if self.heights[k] == 0]
+        nines = [k for k in self.heights if self.heights[k] == 9]
+
+        return [more_itertools.ilen(nx.all_simple_paths(g, start, end)) for start, end in product(zeros, nines)]
 
 
     def part_a(self):
-        zeros = [k for k in self.heights if self.heights[k] == 0]
-        nines = [k for k in self.heights if self.heights[k] == 9]
-
-        total = 0
-        for start, end in itertools.product(zeros, nines):
-            if nx.has_path(self.parsed, start, end):
-                total += 1
-        return total
+        return sum(paths > 0 for paths in self.parsed)
 
     def part_b(self):
-        zeros = [k for k in self.heights if self.heights[k] == 0]
-        nines = [k for k in self.heights if self.heights[k] == 9]
-
-        total = 0
-        for start, end in itertools.product(zeros, nines):
-            a = list(nx.all_simple_paths(self.parsed, start, end))
-            total += len(a)
-        return total
+        return sum(self.parsed)
 
 
 if __name__ == "__main__":
