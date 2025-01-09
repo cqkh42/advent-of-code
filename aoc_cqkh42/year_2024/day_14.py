a4e5e32876147961395ae8a1d718899d1f062ca5
Jack Cooper-McEwan <jackcooper93@gmail.com>
2025-01-09 13:54:12 +0000

    2024 day 14 - hello vectors

--- aoc_cqkh42/year_2024/day_14.py
+++ aoc_cqkh42/year_2024/day_14.py
@@ -13,29 +13,27 @@ class Solution(BaseSolution):
         N_SECS = 10_000
         velocities = np.array([[c,d] for a,b,c,d in self.line_numbers])
         starts = np.array([[a,b] for a, b,c,d in self.line_numbers])
-        # print(starts)
-        seconds = np.array([range(N_SECS) for _ in range(self.num_lines)])
 
-        x_locations = (velocities[:, 0].reshape(-1, 1) * seconds) + np.stack(
-            [starts[:, 0]] * N_SECS, axis=1)
-        x_locations %= 101
+        a = np.repeat(velocities[:, :, np.newaxis], N_SECS, axis=2).cumsum(
+            axis=2) + np.repeat(starts[:, :, np.newaxis], N_SECS, axis=2)
+        mods = np.array([[
+            [101] * N_SECS,
+            [103] * N_SECS
+        ]]*500)
+        a %= mods
 
-        y_locations = (velocities[:, 1].reshape(-1, 1) * seconds) + np.stack(
-            [starts[:, 1]] * N_SECS, axis=1)
-        y_locations %= 103
-
-        tl = np.logical_and(x_locations < 50, y_locations < 51).sum(axis=0)
-        bl = np.logical_and(x_locations < 50, y_locations > 51).sum(axis=0)
-        tr = np.logical_and(x_locations > 50, y_locations < 51).sum(axis=0)
-        br = np.logical_and(x_locations > 50, y_locations > 51).sum(axis=0)
+        tl = np.logical_and(a[:, 0, :] < 50, a[:, 1, :] < 51).sum(axis=0)
+        bl = np.logical_and(a[:, 0, :] < 50, a[:, 1, :] > 51).sum(axis=0)
+        tr = np.logical_and(a[:, 0, :] > 50, a[:, 1, :] < 51).sum(axis=0)
+        br = np.logical_and(a[:, 0, :] > 50, a[:, 1, :] > 51).sum(axis=0)
 
         return tl * tr * br * bl
     def part_a(self):
-        return self.parsed[100]
+        return self.parsed[99]
 
     def part_b(self):
         #uses a hack I found that the christmas tree occurs when the safety factor is minimised
-        return np.argmin(self.parsed)
+        return np.argmin(self.parsed)+1
 
 if __name__ == "__main__":
     submit_answers(Solution,14, 2024)
