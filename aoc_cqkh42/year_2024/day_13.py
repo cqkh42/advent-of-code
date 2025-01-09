65f995687a3cc6b0e2024dd7fed3220af456ce2c
Jack Cooper-McEwan <jackcooper93@gmail.com>
2025-01-09 09:47:02 +0000

    2024 day 13 - fine it works wth the hack

--- aoc_cqkh42/year_2024/day_13.py
+++ aoc_cqkh42/year_2024/day_13.py
@@ -1,3 +1,6 @@
+import itertools
+from typing import Self, Any
+
 import more_itertools
 import numpy as np
 
@@ -6,23 +9,38 @@ from aoc_cqkh42.helpers.base_solution import BaseSolution
 
 
 class Solution(BaseSolution):
+    def _parse(self: Self) -> Any:
+        chunks = more_itertools.chunked(self.line_numbers, 3)
+        return [(np.array([a, b]).T, np.array(c)) for a, b, c in chunks]
+
     def part_a(self):
+        solutions = (np.linalg.inv(arr).dot(c) for arr, c in self.parsed)
+        solutions = (s for s in solutions if np.all(np.isclose(s, s.round())))
+        solutions = ((solution * [3,1]).sum() for solution in solutions)
+        return int(sum(solutions))
+
+    def part_b(self):
+        num = 10000000000000
+        solutions = (np.linalg.inv(arr).dot(c+num) for arr, c in self.parsed)
+        solutions = (s for s in solutions if np.all(np.isclose(s, s.round())))
+        solutions = ((solution * [3,1]).sum() for solution in solutions)
+        return int(sum(solutions))
+        # 1545093008502 is too high
+
         print()
         chunks = more_itertools.chunked(self.line_numbers, 3)
         total = 0
         for a, b, c in chunks:
             arr = np.array([a, b])
+            c = np.array(c) + 10000000000000
+
             solution = np.linalg.inv(arr.T).dot(c)
-            print(a, b, c, solution)
-            if all([np.isclose(num%1, 0) for num in solution]):
+            score = (solution * [3, 1]).sum().round()
 
-                total += (solution * [3,1]).sum()
+            if np.all([np.isclose(num.round(0), num) for num in solution]):
+                print(a, b, c, solution, solution.astype(str))
+                total += score
         return int(total)
-
-
-
-    def part_b(self):
-        ...
 
 
 if __name__ == "__main__":
