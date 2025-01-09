2dbe4ebca8e8298a425912840d22c8da5aaa1097
Jack Cooper-McEwan <jackcooper93@gmail.com>
2025-01-09 09:56:35 +0000

    2024 day 13 - doing it the proper way

--- aoc_cqkh42/year_2024/day_13.py
+++ aoc_cqkh42/year_2024/day_13.py
@@ -3,6 +3,7 @@ from typing import Self, Any
 
 import more_itertools
 import numpy as np
+import sympy
 
 from aoc_cqkh42 import submit_answers
 from aoc_cqkh42.helpers.base_solution import BaseSolution
@@ -11,36 +12,35 @@ from aoc_cqkh42.helpers.base_solution import BaseSolution
 class Solution(BaseSolution):
     def _parse(self: Self) -> Any:
         chunks = more_itertools.chunked(self.line_numbers, 3)
+        out = []
+        for x in chunks:
+            z = zip(*x)
+            out.append(list(list(z)))
+            # print(list(z))
+        return out
         return [(np.array([a, b]).T, np.array(c)) for a, b, c in chunks]
 
     def part_a(self):
-        solutions = (np.linalg.inv(arr).dot(c) for arr, c in self.parsed)
-        solutions = (s for s in solutions if np.all(np.isclose(s, s.round())))
-        solutions = ((solution * [3,1]).sum() for solution in solutions)
-        return int(sum(solutions))
+        a, b = sympy.symbols("a, b", integer=True)
+        total = 0
+        for (x_a, x_b, x_c), (y_a, y_b,y_c) in self.parsed:
+            s = sympy.solve([x_a*a + x_b*b - x_c, y_a*a + y_b*b - y_c], a, b)
+            if s:
+                total += s[a] * 3
+                total += s[b]
+        return total
 
     def part_b(self):
         num = 10000000000000
-        solutions = (np.linalg.inv(arr).dot(c+num) for arr, c in self.parsed)
-        solutions = (s for s in solutions if np.all(np.isclose(s, s.round())))
-        solutions = ((solution * [3,1]).sum() for solution in solutions)
-        return int(sum(solutions))
-        # 1545093008502 is too high
 
-        print()
-        chunks = more_itertools.chunked(self.line_numbers, 3)
+        a, b = sympy.symbols("a, b", integer=True)
         total = 0
-        for a, b, c in chunks:
-            arr = np.array([a, b])
-            c = np.array(c) + 10000000000000
-
-            solution = np.linalg.inv(arr.T).dot(c)
-            score = (solution * [3, 1]).sum().round()
-
-            if np.all([np.isclose(num.round(0), num) for num in solution]):
-                print(a, b, c, solution, solution.astype(str))
-                total += score
-        return int(total)
+        for (x_a, x_b, x_c), (y_a, y_b,y_c) in self.parsed:
+            s = sympy.solve([x_a*a + x_b*b - (x_c+num), y_a*a + y_b*b - (y_c+num)], a, b)
+            if s:
+                total += s[a] * 3
+                total += s[b]
+        return total
 
 
 if __name__ == "__main__":
