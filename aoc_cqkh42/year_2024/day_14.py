005c5866d5f6b7b3973c7bff7d278b0b5f0b3380
Jack Cooper-McEwan <jackcooper93@gmail.com>
2025-01-09 13:28:44 +0000

    2024 day 14 - hello vectors

--- aoc_cqkh42/year_2024/day_14.py
+++ aoc_cqkh42/year_2024/day_14.py
@@ -9,20 +9,33 @@ from aoc_cqkh42.helpers.base_solution import BaseSolution
 
 
 class Solution(BaseSolution):
-    def safety_factor(self, seconds):
-        locations = [((x + (x_v * seconds)) % 101, (y + (y_v * seconds)) % 103) for
-                     x, y, x_v, y_v in self.line_numbers]
-        tl = sum(x < 50 and y < 51 for x, y in locations)
-        bl = sum(x < 50 and y > 51 for x, y in locations)
-        tr = sum(x > 50 and y < 51 for x, y in locations)
-        br = sum(x > 50 and y > 51 for x, y in locations)
+    def _parse(self: Self) -> Any:
+        N_SECS = 10_000
+        velocities = np.array([[c,d] for a,b,c,d in self.line_numbers])
+        starts = np.array([[a,b] for a, b,c,d in self.line_numbers])
+        # print(starts)
+        seconds = np.array([range(N_SECS) for _ in range(self.num_lines)])
+
+        x_locations = (velocities[:, 0].reshape(-1, 1) * seconds) + np.stack(
+            [starts[:, 0]] * N_SECS, axis=1)
+        x_locations %= 101
+
+        y_locations = (velocities[:, 1].reshape(-1, 1) * seconds) + np.stack(
+            [starts[:, 1]] * N_SECS, axis=1)
+        y_locations %= 103
+
+        tl = np.logical_and(x_locations < 50, y_locations < 51).sum(axis=0)
+        bl = np.logical_and(x_locations < 50, y_locations > 51).sum(axis=0)
+        tr = np.logical_and(x_locations > 50, y_locations < 51).sum(axis=0)
+        br = np.logical_and(x_locations > 50, y_locations > 51).sum(axis=0)
+
         return tl * tr * br * bl
     def part_a(self):
-        return self.safety_factor(100)
+        return self.parsed[100]
 
     def part_b(self):
         #uses a hack I found that the christmas tree occurs when the safety factor is minimised
-        return min(range(10_000), key=lambda x: self.safety_factor(x))
+        return np.argmin(self.parsed)
 
 if __name__ == "__main__":
     submit_answers(Solution,14, 2024)
