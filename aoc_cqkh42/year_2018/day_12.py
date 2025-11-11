bc8cb8cc6df332c67077ebf238199b662a8a0820
Jack Cooper-McEwan <jackcooper93@gmail.com>
2025-11-11 23:25:45 +0000

    solving day 12 tweaks

--- aoc_cqkh42/year_2018/day_12.py
+++ aoc_cqkh42/year_2018/day_12.py
@@ -6,28 +6,50 @@ from aoc_cqkh42.helpers.base_solution import BaseSolution
 class Solution(BaseSolution):
     state = ""
     rules = {}
+    valid = set()
+    offset = 0
     def _parse(self: Self) -> Any:
         state, _, *rules = self.lines
         self.state = state.strip("initial state: ")
         for rule in rules:
             left, right = rule.split(" => ")
             self.rules[left] = right
+            if right == "#":
+                self.valid.add(left)
+        new_state = self.state.strip(".")
+        self.offset = len(new_state) - len(self.state)
+        self.state = new_state
 
     def turn(self):
         self.state = f"....{self.state}...."
         new_state = ""
         for index in range(len(self.state)):
             sub = self.state[index:index+5]
-            new_state+= self.rules.get(sub, ".")
+            if sub in self.valid:
+                new_state += "#"
+            else:
+                new_state += "."
         self.state = new_state
+        self.state = self.state.rstrip(".")
+        while self.state.startswith("."):
+            self.offset -= 1
+            self.state = self.state[1:]
+
+        self.offset += 2
     def part_a(self):
         for _ in range(20):
             self.turn()
-        return sum(index for index, value in enumerate(self.state, -40) if value == "#")
+        return sum(index for index, value in enumerate(self.state, -self.offset) if value == "#")
 
         return
 
     def part_b(self):
+        return
+        for index in range(50000000000):
+            self.turn()
+            if not index % 1_000:
+                print(index, self.state)
+        return sum(index for index, value in enumerate(self.state, -40) if value == "#")
         return
 
 if __name__ == "__main__":
