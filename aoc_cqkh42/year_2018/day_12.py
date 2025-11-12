e61ae8d172ea22bd6486ae9866deb296e19aa287
Jack Cooper-McEwan <jackcooper93@gmail.com>
2025-11-12 00:15:33 +0000

    solving day 12 tweaks

--- aoc_cqkh42/year_2018/day_12.py
+++ aoc_cqkh42/year_2018/day_12.py
@@ -1,5 +1,7 @@
 from typing import Self, Any
 
+import more_itertools
+
 from aoc_cqkh42 import submit_answers
 from aoc_cqkh42.helpers.base_solution import BaseSolution
 
@@ -10,42 +12,48 @@ class Solution(BaseSolution):
     def _parse(self: Self) -> Any:
         state, _, *rules = self.lines
         self.state = state.strip("initial state: ")
+        self.state = tuple(char == "#" for char in self.state)
         split = (rule.split(" => ") for rule in rules)
-        self.valid = {left for left, right in split if right == "#"}
-        new_state = self.state.strip(".")
+        valid = (left for left, right in split if right == "#")
+        self.valid = {tuple(char == "#" for char in rule) for rule in valid}
+        new_state = tuple(more_itertools.strip(self.state, lambda x: not x))
         self.offset = len(new_state) - len(self.state)
         self.state = new_state
 
     def turn(self):
-        self.state = f"....{self.state}...."
-        new_state = ""
-        for index in range(len(self.state)):
-            sub = self.state[index:index+5]
-            if sub in self.valid:
-                new_state += "#"
-            else:
-                new_state += "."
-        self.state = new_state
-        self.state = self.state.rstrip(".")
-        while self.state.startswith("."):
+        self.state = (False, False, False, False, *self.state, False, False, False)
+        self.state   = tuple(phrase in self.valid for phrase in more_itertools.windowed(self.state, 5, fillvalue=False))
+        self.state = tuple(more_itertools.rstrip(self.state, lambda x: not x))
+        while not self.state[0]:
             self.offset -= 1
-            self.state = self.state[1:]
+            self.state = tuple(self.state[1:])
 
         self.offset += 2
     def part_a(self):
+        return
         for _ in range(20):
             self.turn()
-        return sum(index for index, value in enumerate(self.state, -self.offset) if value == "#")
+        return sum(index for index, value in enumerate(self.state, -self.offset) if value)
 
         return
 
     def part_b(self):
-        return
-        for index in range(50000000000):
+        # y = 96x + c
+        # 48001087 = 96*500000 + c 1087
+        # 28801087 = 96*300000+ c
+        # increases 96 every turn
+        # need an extra 49999290000
+        # return
+        for index in range(1, 50000000001):
+        # for index in range(580000):
             self.turn()
-            if not index % 1_000:
-                print(index, self.state)
-        return sum(index for index, value in enumerate(self.state, -40) if value == "#")
+            if not index % 100_000:
+                print()
+                str = "".join("#" if char else "." for char in self.state)
+
+                total = sum(index for index, value in enumerate(self.state, -self.offset) if value)
+                print(index, len(self.state), total)
+        return sum(index for index, value in enumerate(self.state, -self.offset) if value)
         return
 
 if __name__ == "__main__":
