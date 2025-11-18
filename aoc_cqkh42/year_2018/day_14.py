6e02d08a6be2c435473f380b5a42a1e8b5930fa2
Jack Cooper-McEwan <jackcooper93@gmail.com>
2025-11-18 17:06:25 +0000

    solving day 14 speedy

--- aoc_cqkh42/year_2018/day_14.py
+++ aoc_cqkh42/year_2018/day_14.py
@@ -4,35 +4,41 @@ from collections import Counter
 from aoc_cqkh42 import submit_answers
 from aoc_cqkh42.helpers.base_solution import BaseSolution
 import re
+from collections import deque
 
 class Solution(BaseSolution):
-    recipes = [3, 7]
-    elves = [0, 1]
+    recipes = [3,7]
+    elf_1 = 0
+    elf_2 = 1
+    count = 2
+    def step(self):
+        a, b = divmod(self.recipes[self.elf_1] + self.recipes[self.elf_2], 10)
+        valid = (a, b) if a else (b,)
+        self.count += len(valid)
+        self.recipes.extend(valid)
+        self.elf_1 = (1 + self.elf_1 + self.recipes[self.elf_1]) % self.count
+        self.elf_2 = (1 + self.elf_2 + self.recipes[self.elf_2]) % self.count
+        return valid
+
     def part_a(self, steps: int = None) -> str:
         steps = steps or self.number
-        while len(self.recipes) < steps+10:
-            a, b = divmod(self.recipes[self.elves[0]] +self.recipes[self.elves[1]], 10)
-            if a:
-                self.recipes.append(a)
-            self.recipes.append(b)
-            self.elves[0] = (1+self.elves[0] + self.recipes[self.elves[0]])%len(self.recipes)
-            self.elves[1] = (1+self.elves[1] + self.recipes[self.elves[1]])%len(self.recipes)
+        while self.count < steps+10:
+            self.step()
         return "".join(str(num) for num in self.recipes[steps:steps+10])
 
     def part_b(self):
+        # secondly, try a dict look up
         recipe_str = "".join(str(num) for num in self.recipes)
-        for _ in range(50_000_000):
-            a, b = divmod(self.recipes[self.elves[0]] + self.recipes[self.elves[1]], 10)
-            if a:
-                self.recipes.extend([a, b])
-                recipe_str += f"{a}{b}"
+        if self.input_ in recipe_str:
+            raise NotADirectoryError
+        recipe_str = recipe_str[-10:]
+        while self.input_ not in recipe_str:
+            added = self.step()
+            if len(added) == 1:
+                recipe_str = f"{recipe_str[-9:]}{added[0]}"
             else:
-                self.recipes.extend([b])
-                recipe_str += str(b)
-            self.elves[0] = (1 + self.elves[0] + self.recipes[self.elves[0]]) % len(self.recipes)
-            self.elves[1] = (1 + self.elves[1] + self.recipes[self.elves[1]]) % len(self.recipes)
-        a = re.sub(f"{self.input_}.*", "", recipe_str)
-        return len(a)
+                recipe_str = f"{recipe_str[-8:]}{added[0]}{added[1]}"
+        return self.count - (len(self.input_) + (not recipe_str.endswith(self.input_)))
 
 if __name__ == "__main__":
     submit_answers(Solution, 14, 2018)
